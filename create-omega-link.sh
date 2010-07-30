007ba232c46adc17ff165e9f4421e12485f25913
Miki Tebeka <miki@saucelabs.com>
2010-07-30 19:29:24 +0000

    Create link to Jason will be happy

--- create-omega-link.sh
+++ create-omega-link.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Make symbolic links to create working environment
 
-ln -s /usr/lib/cgi-bin/omega/omega search.cgi
+cd $(dirname $0)
 
+ln -s /usr/lib/cgi-bin/omega/omega search.cgi
+root=$PWD
+mkdir irc
+(cd irc && ln -s $root/logs/ChannelLogger logs)
