1ad567adb8cfc9d8199d4bed3c6fe5b1fa8b8a58
Miki Tebeka <miki@saucelabs.com>
2010-07-30 18:23:22 -0700

    Merged to install.sh

--- create-omega-link.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Make symbolic links to create working environment
-
-cd $(dirname $0)
-
-ln -s /usr/lib/cgi-bin/omega/omega index.cgi
-root=$PWD
-mkdir irc
-(cd irc && ln -s $root/logs/ChannelLogger logs)
